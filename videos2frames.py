e5edd24411974fd8b50b3eab659ec57166419b59
Osvaldo Cartagena <osvaldocartagena@MacBook-Air-de-Osvaldo.local>
2020-11-20 16:58:51 -0300

    se modifica vdeos2frames y se agrega mov_duckiebot

--- videos2frames.py
+++ videos2frames.py
@@ -5,7 +5,7 @@ root='/Users/osvaldocartagena/logs/videos'
 
 ls=os.listdir(root)
 
-for vname in ls[:2]:
+for vname in ls:
     k=0
     cap = cv2.VideoCapture(root+'/'+vname)
     print(root+'/'+vname)
@@ -16,9 +16,9 @@ for vname in ls[:2]:
         ret, frame = cap.read()
         if ret==False:
             break
-        if k%10==0:
-            cv2.imwrite('/Users/osvaldocartagena/frames/'+vname+str(int(k/10))+'.png', frame)
-            print('frames/'+vname+str(int(k/10))+'.png')
+        if k%60==0:
+            cv2.imwrite('/Users/osvaldocartagena/frames/'+vname+str(int(k/10))+'.jpg', frame)
+            print('frames/'+vname+str(int(k/60))+'.png')
         k=k+1
 
     cap.release()
